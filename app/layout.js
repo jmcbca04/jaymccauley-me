243b5dec7f973692dca100c25d2b258052429910
Jay McCauley <jay.a.mccauley@gmail.com>
2024-12-14 18:29:10 -0500

    added OG image

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,28 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Jay McCauley',
   description: 'Personal website of Jay McCauley',
+  openGraph: {
+    title: 'Jay McCauley',
+    description: 'Personal website of Jay McCauley',
+    url: 'https://jaymccauley.me',
+    siteName: 'Jay McCauley',
+    images: [
+      {
+        url: '/jaymccauley-og.png',
+        width: 1200,
+        height: 630,
+        alt: 'Jay McCauley',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Jay McCauley',
+    description: 'Personal website of Jay McCauley',
+    images: ['/jaymccauley-og.png'],
+  },
 }
 
 export default function RootLayout({ children }) {
