378ee7cf6c2a1965c6ff4c60e2ce9ac1a7e851cb
Jay McCauley <jay.a.mccauley@gmail.com>
2024-12-14 18:54:18 -0500

    removed OG optimization stuff

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'dist',
   images: {
     unoptimized: true
-  },
-  reactStrictMode: true,
-  // Only process specific directories
-  onDemandEntries: {
-    maxInactiveAge: 25 * 1000,
-    pagesBufferLength: 2,
-  },
-  // Explicitly include only the directories we need
-  transpilePackages: ['@vercel/analytics'],
-  experimental: {
-    optimizeCss: true,
-    legacyBrowsers: false
   }
 }
 
